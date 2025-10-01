a4d39b479a73cc2ca85a2fd3f4392d0fd6709eaa
Ruan Sousa <ruansousad4c@gmail.com>
2025-10-01 20:50:44 -0300

    feat: preencher banco com modelos básicos de palavras

--- sql/schema.sql
+++ sql/schema.sql
@@ -21,8 +21,7 @@ create table worker_entity(
 
 create table word_types(
 	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	word_type TEXT not null
-);
+	word_type TEXT not null );
 
 create table words(
 	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -32,3 +31,11 @@ create table words(
 );
 
 insert into word_types(word_type) values ('pronome'),('substantivo'), ('verbo'), ('adjetivo'), ('adverbio');
+
+insert into words(word, word_type_id)
+values ('eu', 1), ('você', 1), ('vocês', 1),('nós', 1), ('eles', 1), ('elas', 1)
+insert into words(word, word_type_id) 
+values ('água', 2), ('máquinas', 2), ('humanos', 2), ('matemática', 2), ('computação', 2), ('ciência', 2), ('engenharia', 2), ('vida', 2), ('morte', 2);
+insert into words(word, word_type_id)
+values ('amar', 3), ('estudar', 3), ('odiar', 3), ('viver', 3), ('escolher', 3), ('vencer', 3), ('morrer', 3), ('destruir', 3), ('pensar', 3), ('enganar', 3), ('ajudar', 3), ('ser', 3);
+select * from words
